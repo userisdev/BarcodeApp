cba74ae4a2cb59fc7a6d8a2471951413f67dfd25
Kento Ooiwa <lunatic.heros@gmail.com>
2024-03-10 02:06:28 +0900

    first commit.

---
+++ BarcodeApp/BarcodeResult.cs
@@ -0,0 +1,43 @@
+﻿using SkiaSharp;
+using ZXing.Common;
+using ZXing;
+using ZXing.SkiaSharp;
+
+/// <summary> BarcodeResult recod. </summary>
+/// <seealso cref="System.IEquatable&lt;TABBotApp.Libs.Workers.Barcode.BarcodeResult&gt;" />
+internal sealed record BarcodeResult(string Text, bool HasValue)
+{
+    /// <summary> Loads the specified path. </summary>
+    /// <param name="path"> The path. </param>
+    /// <returns> </returns>
+    public static BarcodeResult Load(string path)
+    {
+        using SKBitmap bmp = SKBitmap.Decode(path);
+        if (bmp is null)
+        {
+            Console.WriteLine("image decode error.");
+        }
+
+        BarcodeReader reader = new()
+        {
+            AutoRotate = true,
+            Options = new DecodingOptions
+            {
+                TryInverted = true,
+                TryHarder = true,
+                PureBarcode = false,
+                PossibleFormats = [BarcodeFormat.EAN_13]
+            }
+        };
+
+        Result? result = reader.Decode(bmp);
+        return result is null ? new BarcodeResult(string.Empty, false) : new BarcodeResult(result.Text, true);
+    }
+
+    /// <summary> Converts to string. </summary>
+    /// <returns> </returns>
+    public override string ToString()
+    {
+        return HasValue ? Text : "[error]";
+    }
+}
---
+++ BarcodeApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using SkiaSharp;
+
+var imagePath = args.FirstOrDefault();
+if (!File.Exists(imagePath))
+{
+    Console.WriteLine("not found.");
+    Console.ReadKey(true);
+    Environment.Exit(1);
+}
+
+var data = File.ReadAllBytes(imagePath);
+Console.WriteLine($"size={data.Length}");
+using var bmp = SKBitmap.Decode(data);
+var result = BarcodeResult.Load(imagePath);
+
+Console.WriteLine($"result={result}");
+
+Console.WriteLine("press any key to exit . . .");
+Console.ReadKey(true);
+Environment.Exit(0);
